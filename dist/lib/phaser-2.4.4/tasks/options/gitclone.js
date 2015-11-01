ac29b7944f14c2e9cee16144e10ab48f86eb5139
Andrew Yan <zushenyan@gmail.com>
2015-11-01 23:48:56 +0800

    update that update this

--- dist/lib/phaser-2.4.4/tasks/options/gitclone.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: {
-        options: {
-            repository: "https://github.com/photonstorm/phaser-plugins",
-            branch: "master",
-            directory: "out/plugins"
-        }
-    }
-};
